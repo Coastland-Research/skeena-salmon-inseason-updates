af06d0db0a5dfa40fda2f6bf949c710d51f6b6ba
andy <andy@coastlandresearch.com>
2025-06-26 20:52:24 -0700

    update index data and add line to trtc plot

--- scripts/plot_sx_trtc_model.R
+++ scripts/plot_sx_trtc_model.R
@@ -2,9 +2,10 @@
 make.trtc.sx.plot<-function(data) {
   
 ggplot(data,aes(x=Date,y=Estimate,color=Timing))+
+    geom_hline(yintercept=2400000,linetype="dashed")+
     geom_line(linewidth=1.5)+
     theme_bw()+
     theme(legend.position="bottom")+
     labs(y="Sockeye TRTC Estimate at Tyee")
-
+    
 }
