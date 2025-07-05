e15d99be056c067e890849665ca8db5e84fba322
andy <andy@coastlandresearch.com>
2025-07-05 14:15:44 -0700

    july 5 update

--- scripts/plot_sx_trtc_model.R
+++ scripts/plot_sx_trtc_model.R
@@ -1,10 +1,11 @@
 
-make.trtc.sx.plot<-function(data) {
+make.trtc.sx.plot<-function(data,yhigh) {
   
 ggplot(data,aes(x=Date,y=Estimate,color=Timing))+
     geom_hline(yintercept=2400000,linetype="dashed")+
     geom_line(linewidth=1.5)+
     theme_bw()+
+    ylim(0,yhigh)+
     theme(legend.position="bottom")+
     labs(y="Sockeye TRTC Estimate at Tyee")
     
