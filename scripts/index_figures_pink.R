cef580a49a92ea115772bb9d29563a50223b019c
elizabeth-hirsch <ehirsch@unbc.ca>
2025-07-11 10:45:48 -0700

    fix odd-year pinks figures

---
+++ scripts/index_figures_pink.R
@@ -0,0 +1,63 @@
+#create index figures with a filter for species
+
+make.index.figures.pink <-function(daily,current,yhigh,y.cum.high,xhigh) {
+  
+  daily.index<-left_join(daily,current,by="Date")%>%
+    mutate(Date=as.Date(Date))
+  
+  gg.daily<-daily.index%>%
+    mutate_if(is.character, as.numeric) %>%
+    pivot_longer(`1957`:`2025`,names_to="Year",values_to="Fish") %>%
+    mutate(Year=as.numeric(Year)) %>%
+    mutate(Index=replace_na(Fish,0))
+  
+  gg.daily.quants<-gg.daily %>%
+    group_by(Date) %>%
+    summarise(per10 = quantile(Fish,.1, na.rm = TRUE), 
+              per25 = quantile(Fish,.25, na.rm = TRUE), 
+              per50 = quantile(Fish,.50, na.rm = TRUE),
+              per75 = quantile(Fish,.75, na.rm = TRUE),
+              per90 = quantile(Fish,.9, na.rm = TRUE))%>%
+    select(Date,per10,per25,per50,per75,per90)%>%
+    pivot_longer("per10":"per90",names_to="Q",values_to="Index")%>%
+    group_by(Q)%>%
+    mutate(Fish=(Index))%>%
+    mutate(qgroup=case_when(Q=="per10"|Q=="per90"~"10/90th",
+                            Q=="per25"|Q=="per75"~"25/75th",
+                            Q=="per50"~"Median"))
+  
+  years_out<-c("2008","2009","2010")
+  
+  gg.daily<-gg.daily%>%filter(!Year%in%years_out)
+  
+  gg.daily.cum<-daily.index%>%
+    pivot_longer(`1957`:`2025`,names_to="Year",values_to="Fish") %>%
+    group_by(Year)%>%
+    mutate(cum_sum=cumsum(replace_na(Fish,0)))
+  
+  gg.daily.cum.quants<-gg.daily.cum %>%
+    group_by(Date) %>%
+    summarise(per10 = quantile(Fish,.1, na.rm = TRUE), 
+              per25 = quantile(Fish,.25, na.rm = TRUE), 
+              per50 = quantile(Fish,.50, na.rm = TRUE),
+              per75 = quantile(Fish,.75, na.rm = TRUE),
+              per90 = quantile(Fish,.9, na.rm = TRUE))%>%
+    select(Date,per10,per25,per50,per75,per90)%>%
+    pivot_longer("per10":"per90",names_to="Q",values_to="Index")%>%
+    group_by(Q)%>%
+    mutate(cum_sum=cumsum(Index))%>%
+    mutate(qgroup=case_when(Q=="per10"|Q=="per90"~"10/90th",
+                            Q=="per25"|Q=="per75"~"25/75th",
+                            Q=="per50"~"Median"))
+  
+  years_out<-c("2008","2009","2010")
+  
+  gg.daily.cum<-gg.daily.cum%>%filter(!Year%in%years_out)
+  
+  daily.index<-make.daily.index.plot.pink(gg.daily,gg.daily.quants,xhigh,yhigh)
+  cum.index<-make.cum.index.plot.pink(gg.daily.cum,gg.daily.cum.quants,xhigh,y.cum.high)
+  
+  #Daily plot with 2025 and quants
+  ggarrange(daily.index,cum.index,align="v",ncol=1,common.legend = TRUE,legend="bottom")
+  
+}
---
+++ scripts/plot_index_figures_pink.R
@@ -0,0 +1,38 @@
+#daily and cumulative index plots
+
+make.daily.index.plot.pink<-function(daily.data,daily.quants,xhigh,yhigh) {
+  
+  ggplot(daily.data,aes(x=Date, y=Fish,group=Date))+
+    geom_line(data=daily.quants,aes(colour=qgroup,group=Q),linetype="longdash",linewidth=1)+
+    geom_line(data = daily.data %>% filter(Year == 2025),
+              aes(x = Date, y = Fish, group = 1, color = "2025 Data"),
+              linewidth = 1.5,alpha=.7)+
+    geom_line(data = daily.data %>% filter(Year <2025), aes(x = Date, y = Fish,group=Year),
+              linewidth = .5,alpha=.1)+
+    scale_color_manual(values=c("grey75","purple","grey50","black"))+
+    labs(y="Daily Index",color="")+
+    theme_bw()+
+    theme(legend.position="bottom",axis.title.x=element_blank())+
+    ylim(0,yhigh)+
+    xlim(as.Date("2025-06-03"),xhigh)
+}
+
+make.cum.index.plot.pink <-function(cum.data,cum.quants,xhigh,y.cum.high){
+  
+  ggplot(cum.data,aes(x=Date, y=cum_sum,group=Date))+
+    geom_line(data=cum.quants,aes(colour=qgroup,group=Q),linetype="longdash",linewidth=1)+
+    geom_line(data = cum.data %>% filter(Year == 2025&Date<=tyee.day),
+              aes(x = Date, y = cum_sum, group = 1,color = "2025 Data"),
+              linewidth = 1.5,alpha=.7)+
+    geom_line(data = cum.data %>% filter(Year <2025), aes(x = Date, y = cum_sum,group=Year),
+              linewidth = .5,alpha=.1)+
+    scale_color_manual(values=c("grey75","purple","grey50","black"))+
+    labs(y="Cumulative Daily Index",color="")+
+    theme_bw()+
+    theme(legend.position = "bottom")+
+    xlim(as.Date("2025-06-03"),xhigh)+
+    ylim(0,y.cum.high)
+  
+}
+
+
