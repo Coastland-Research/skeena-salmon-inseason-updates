efb3256d2e9370183d4f18bda23956bbbda5a78a
elizabeth-hirsch <ehirsch@unbc.ca>
2026-07-02 09:50:32 -0700

    7/2 updates

--- make_rec_plots.R
+++ make_rec_plots.R
@@ -1,15 +1,14 @@
 
 creel_catch <- read.csv("data/current_year/recreational creel/rec_creel_catch.csv") %>%
-  mutate(Month = factor(Month, levels = c("May", "June", "July", "August")))%>%
-  rename(Estimate = Value)
+  mutate(Month = factor(Month, levels = c("May", "June", "July", "August")))
          
 creel_effort <- read.csv("data/current_year/recreational creel/rec_creel_effort.csv")
 
 effort_long <- creel_effort %>%
   mutate(Disposition = "Effort",
          Species = NA_character_) %>%
-  rename(Estimate = Effort) %>%
-  select(Year, Species, Month, Disposition, Estimate)
+  rename(Value = Effort) %>%
+  select(Year, Species, Month, Disposition, Value)
 
 species <- unique(creel_catch$Species)
 
@@ -20,9 +19,8 @@ effort_long <- tidyr::crossing(
 combined_creel <- bind_rows(creel_catch, effort_long)
 
 (chinook_creel <- combined_creel %>%
-    mutate(Month = forcats::fct_rev(Month)) %>%
   filter(Species == "Chinook") %>%
-  ggplot(aes(x = Year, y = Estimate, fill = Month))+
+  ggplot(aes(x = Year, y = Value, fill = Month))+
   geom_col()+
   scale_fill_brewer(palette = "Spectral")+
   ggtitle("Chinook Salmon")+
@@ -32,9 +30,8 @@ combined_creel <- bind_rows(creel_catch, effort_long)
   scale_x_continuous(breaks = seq(2015, 2026, by = 1)))
 
 (coho_creel <- combined_creel %>%
-    mutate(Month = forcats::fct_rev(Month)) %>%
   filter(Species == "Coho") %>%
-  ggplot(aes(x = Year, y = Estimate, fill = Month))+
+  ggplot(aes(x = Year, y = Value, fill = Month))+
   scale_fill_brewer(palette = "Spectral")+
   geom_col()+
   ggtitle("Coho Salmon")+
@@ -44,9 +41,8 @@ combined_creel <- bind_rows(creel_catch, effort_long)
   scale_x_continuous(breaks = seq(2015, 2026, by = 1)))
 
 (halibut_creel <- combined_creel %>%
-    mutate(Month = forcats::fct_rev(Month)) %>%
   filter(Species == "Pacific Halibut") %>%
-  ggplot(aes(x = Year, y = Estimate, fill = Month))+
+  ggplot(aes(x = Year, y = Value, fill = Month))+
   geom_col()+
   scale_fill_brewer(palette = "Spectral")+
   ggtitle("Pacific Halibut")+
