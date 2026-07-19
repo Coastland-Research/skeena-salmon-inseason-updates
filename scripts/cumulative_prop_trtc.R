66c56d95a2eaefbaec16f623e36f8a360178ebfa
Coastland <andy@coastlandresearch.com>
2026-07-19 08:13:20 -0700

    july 19 update, turned on cum prop sockeye figure, updated text etc.

--- scripts/cumulative_prop_trtc.R
+++ scripts/cumulative_prop_trtc.R
@@ -9,16 +9,17 @@ gg.daily.pcum<-daily_26%>%select("Date":"2025")%>%
   mutate(Year=as.numeric(Year), Date = as.Date(Date))
 
 data <- read.csv("data/current_year/tyee data 2026.csv") %>%
-  mutate(Date = as.Date(Date)) %>%
-  mutate(Date = as.Date(gsub("2026", "2025", Date)))
+  mutate(Date = as.Date(Date))
+ # mutate(Date = as.Date(gsub("2026", "2025", Date)))
 
 # read in data 
 data2<-data.frame(date=data$Date,cumtrtc=data$cum_check)%>%
   mutate(`1M`=cumtrtc/1000000,
          `1.5M`=cumtrtc/1500000,
          `2M`=cumtrtc/2000000,
-         `2.5M`=cumtrtc/2500000)%>%
-  pivot_longer(`1M`:`2.5M`,names_to="estimate",values_to="p")%>%
+         `2.5M`=cumtrtc/2500000,
+         `3M`=cumtrtc/3000000)%>%
+  pivot_longer(`1M`:`3M`,names_to="estimate",values_to="p")%>%
   select(-cumtrtc)
 
 
@@ -26,14 +27,17 @@ make.prop.figure<-function(gg.daily.pcum, data2) {
   
   ggplot(gg.daily.pcum,aes(x=Date,y=Sockeye,group=Date))+
     geom_boxplot(fill="white",color="grey70",alpha=.5)+
-    geom_line(data = data2, aes(x=date,y=p,color=estimate,group=estimate),linewidth=1,linewidth=1) +
-    geom_segment(aes(x=as.Date("2025-06-01"),y=.5,xend=as.Date("2025-07-24"),yend=.5),color="blue",linewidth=1,linetype="dashed")+
-    geom_segment(aes(x=as.Date("2025-07-24"),y=.5,xend=as.Date("2025-07-24"),yend=0),color="blue",arrow = arrow(length=unit(.25, 'cm')),linewidth=1,linetype="dashed")+
+    geom_line(data = data2, aes(x=date,y=p,color=estimate,group=estimate),linewidth=1) +
+    geom_segment(aes(x=as.Date("2026-06-01"),y=.5,xend=as.Date("2026-07-24"),yend=.5),color="blue",linewidth=1,linetype="dashed")+
+    geom_segment(aes(x=as.Date("2026-07-24"),y=.5,xend=as.Date("2026-07-24"),yend=0),color="blue",arrow = arrow(length=unit(.25, 'cm')),linewidth=1,linetype="dashed")+
     #ylim(0,150000)+
     theme_bw()+
     ylim(0,1)+
+    expand_limits(x=c(as.Date("2026-06-01"),as.Date("2026-10-01")))+
     labs(color="Estimate",y="Cumulative Proportion of TRTC")+
     theme(legend.position = "bottom")
   
 }
+
+make.prop.figure(gg.daily.pcum, data2)
 
