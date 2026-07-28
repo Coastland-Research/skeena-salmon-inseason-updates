a54a7811f482345a58177a32dc69ba23a465ae38
elizabeth-hirsch <ehirsch@unbc.ca>
2026-07-28 14:25:49 -0700

    change Babine figure titles to Count

---
+++ scripts/cumulative_count_babine.R
@@ -0,0 +1,56 @@
+
+# BABINE - historical time series for cumulative index by date with 10yr rolling average
+#daily<-daily
+#current<-current
+
+# Use "count" instead of "index" for Babine fence
+
+make.hist.index.babine <-function(daily,current, fence.day) {
+  
+  daily.index<-left_join(daily,current,by="Date")%>%
+    mutate(Date=as.Date(Date))
+  
+  long_dat <- daily.index %>%
+    pivot_longer(cols = matches("^\\d{4}$"),
+                 names_to = "Year",
+                 values_to = "Count") %>%
+    mutate(Year = as.numeric(Year),Count = as.numeric(Count)) %>%
+    arrange(Year, MONTH, DAY) %>%
+    group_by(Year) %>%
+    mutate(Cumulative_Count = cumsum(replace_na(Count, 0))) %>%
+    ungroup()
+  
+  plot_data <- long_dat %>%
+    filter(Date == tyee.day)
+  
+  plot_data <- plot_data %>%
+    arrange(Year) %>%
+    mutate(rolling_10yr = rollmean(lag(Cumulative_Count), k = 10,align = "right",fill = NA))
+  
+  avg5 <- plot_data %>%
+    filter(Year >= max(Year) - 5 & Year < max(Year)) %>%
+    summarise(avg = mean(Cumulative_Count)) %>%
+    pull(avg)
+  
+  ggplot(plot_data, aes(x = Year, y = Cumulative_Count, fill = Year == 2026)) +
+    geom_col()+
+    scale_fill_manual(
+      values = c("TRUE" = "purple",
+                 "FALSE" = "steelblue"),
+      guide = "none")+
+    geom_segment(
+      aes(x = max(Year) - 5,
+          xend = max(Year)-1,
+          y = avg5,
+          yend = avg5),
+      colour = "red",
+      linewidth = 0.9)+
+    geom_line(aes(y = rolling_10yr),
+              colour = "black",
+              linewidth = 1.2) +
+    theme_bw() +
+    scale_x_continuous(breaks=seq(1950, 2026, by = 5))+
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))+
+    labs(x = "Year", y = paste("Cumulative Fish Count through", tyee.date))
+}
+
