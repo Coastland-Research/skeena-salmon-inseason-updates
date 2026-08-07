cb99ea2ba539d77f1ab82dfd7d43509eb3260a2e
elizabeth-hirsch <ehirsch@unbc.ca>
2026-08-07 16:25:42 -0700

    add code for Babine vs Tyee fence counts for years 2000-2025

--- scripts/babine versus tyee.R
+++ scripts/babine versus tyee.R
@@ -13,12 +13,12 @@
 #library(dplyr)
 #library(data.table)
 
-#pernonbabine<-.1
-#p.catch.babine<-.97
-#ylow<--150000
-#yhigh<-500000
-#start.date<-as.Date("2026-06-01")
-#end.date<-as.Date("2026-09-01")
+pernonbabine<-.1
+p.catch.babine<-.97
+ylow<--150000
+yhigh<-500000
+start.date<-as.Date("2026-06-01")
+end.date<-as.Date("2026-09-01")
 
 make.tyee.vs.babine.plot<-function(pernonbabine,p.catch.babine,ylow,yhigh,start.date,end.date) {
 
@@ -67,5 +67,68 @@ ggplot(comp,aes(x=Date,y=Fish,color=Count,size=Count,linetype=Count)) +
   labs(y="Number of Large Sockeye") +
   ylim(ylow,yhigh)+
   xlim(start.date,end.date)
-
 }
+
+
+# Babine vs Tyee for last 25 years ----------------------------------------
+
+# 1. Load the historical daily Tyee run data
+tyee <- fread("data/common/tyee daily escapement data.csv") %>%
+  pivot_longer(cols = c(`1970`:`2025`), names_to = "Year", values_to = "Tyee_Fish") %>%
+  mutate(Year = as.integer(Year)) %>%
+  filter(Year >= 2000) %>%  #filter to years since 2000
+  group_by(Year) %>%
+  mutate(Babine_est = lag(Tyee_Fish, n = 21)*0.9) %>% #expected Tyee fish at Babine 3 weeks later
+  ungroup()
+
+# 4. Load Babine data since 2000 and pivot longer by year
+babine <- fread("data/common/babine_daily_sockeye_historical.csv") %>%
+  pivot_longer(cols = c(`1946`:`2025`), names_to = "Year", values_to = "Babine_Fish") %>%
+  mutate(Year = as.integer(Year)) %>%
+  filter(Year >= 2000) %>%
+  mutate(Date = as.IDate(Date, format = "%m/%d/%y"))
+  # mutate(`month-day` = paste(MONTH, DAY, sep = "-"))
+
+# 5. merge the data by year and day
+combined <- left_join(babine,tyee, by = c("Year", "Date"))%>%
+  rename(Month = MONTH, Day = DAY)
+
+# 6. ggplot (actual Tyee, expected Babine, actual Babine, 
+# and actual Babine minus the expected Babine) and use facet_wrap to show each year
+
+comp <- combined %>%
+  arrange(Year, Month, Day) %>%
+  group_by(Year) %>%
+  mutate(
+    Tyee_Fish = replace_na(Tyee_Fish, 0),
+    Babine_Fish = replace_na(Babine_Fish, 0),
+    Expected_daily = lag(Tyee_Fish * 0.9, 21),
+    Observed.Tyee = cumsum(Tyee_Fish),
+    Actual.Fence = cumsum(Babine_Fish),
+    Expected.Babine = cumsum(replace_na(Expected_daily, 0)),
+    `Difference @ 3 Weeks` = Actual.Fence - Expected.Babine) %>%
+  ungroup() %>%
+  pivot_longer(cols = c(Observed.Tyee, Actual.Fence, Expected.Babine, `Difference @ 3 Weeks`),
+    names_to = "Count",values_to = "Fish") %>%
+  mutate(Fish = na_if(Fish, 0), Date = as.Date(Date))
+
+# Plot
+ggplot(comp,aes(Date, Fish, colour = Count, linetype = Count, linewidth = Count)) +
+  geom_line() +
+  facet_wrap(~Year) +
+  theme_bw() +
+  scale_colour_manual(values = c("Actual.Fence" = "black",
+    "Difference @ 3 Weeks" = "red",
+    "Expected.Babine" = "seagreen4",
+    "Observed.Tyee" = "blue")) +
+  scale_linetype_manual(values = c("Actual.Fence" = "solid",
+    "Difference @ 3 Weeks" = "solid",
+    "Expected.Babine" = "dashed",
+    "Observed.Tyee" = "solid")) +
+  scale_linewidth_manual(values = c("Actual.Fence" = 1.5,
+    "Difference @ 3 Weeks" = 1,
+    "Expected.Babine" = 1,
+    "Observed.Tyee" = 1.5)) +
+  theme_bw() +
+  theme(legend.position = "top")
+
